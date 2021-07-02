b6b2f3a6d4d4f37700d0b7487c65cdc124fbc17c
nazordz <nopalnaxcpm@gmail.com>
2021-07-02 16:26:41 +0700

    Finish layout

---
+++ src/main/java/javaLesson1/Main.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaLesson1;
+
+import javaLesson1.Views.DashboardFrame;
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author mac
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        JFrame dashboard = new DashboardFrame();
+        dashboard.setVisible(true);
+
+    }
+    
+}
